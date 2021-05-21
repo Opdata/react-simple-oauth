f64bec52cb9e27aacd9177bf5d514363d3f66a56
Opdata <pb03641@gmail.com>
2021-05-21 13:55:58 +0900

    Install Mongoose and Connect Test

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
+
+mongoose.connect(
+  'mongodb+srv://root:root@cluster0.c2z7m.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  () => {
+    console.log('Connected Mongoose Success');
+  }
+);
 
 app.get('/', (req, res) => {
   res.send('hello world');
