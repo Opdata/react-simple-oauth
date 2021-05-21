d373d7e9443a75f94d66d726d1a3a65bcea1db6e
Opdata <pb03641@gmail.com>
2021-05-21 17:35:52 +0900

    LoginPage Components Styling

--- client/src/Components/LoginPage/Loginpage.tsx
+++ client/src/Components/LoginPage/Loginpage.tsx
@@ -1,3 +1,15 @@
+import googleIcon from '../../Assets/googleIcon.png';
+import styles from './Loginpage.module.css';
 export default function Loginpage() {
-  return <div>Login Page</div>;
+  return (
+    <div className={styles.loginPage}>
+      <h1>Login Page</h1>
+      <div className={styles.loginForm}>
+        <div className={styles.googleContainer}>
+          <img src={googleIcon} alt="google Icon" />
+          <p>Login With Google</p>
+        </div>
+      </div>
+    </div>
+  );
 }
