8f0fb16cbdc5b8d70aae3508b9eed6b1931b3e2c
Opdata <pb03641@gmail.com>
2021-05-25 16:38:46 +0900

    Import Context & axios Request Option(Credentials) Add

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,13 @@
+import { useContext } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { myContext } from './Components/Context';
 import Homepage from './Components/HomePage/Homepage';
 import Loginpage from './Components/LoginPage/Loginpage';
 import Navbar from './Components/NavBar/Navbar';
 
 function App() {
+  const useObj = useContext(myContext);
+  console.log(useObj);
   return (
     <BrowserRouter>
       <Navbar />
--- client/src/Components/Context.tsx
+++ client/src/Components/Context.tsx
@@ -1,17 +1,19 @@
 import { useState, useEffect, createContext } from 'react';
 import axios, { AxiosResponse } from 'axios';
 
-const myContext = createContext({});
+export const myContext = createContext({});
 
 export default function Context(props: any) {
   const [userObj, setUserObj] = useState<any>();
 
   useEffect(() => {
-    axios.get('http://localhost:4000/getuser').then((res: AxiosResponse) => {
-      if (res.data) {
-        setUserObj(res.data);
-      }
-    });
+    axios
+      .get('http://localhost:4000/getuser', { withCredentials: true })
+      .then((res: AxiosResponse) => {
+        if (res.data) {
+          setUserObj(res.data);
+        }
+      });
   }, []);
 
   return (
