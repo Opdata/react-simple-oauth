ac1760f748e783abd921961b9aa93e0e56556512
Opdata <pb03641@gmail.com>
2021-05-25 16:17:50 +0900

    Axios Request Code & Context Hook Code Add

---
+++ client/src/Components/Context.tsx
@@ -0,0 +1,20 @@
+import { useState, useEffect, createContext } from 'react';
+import axios, { AxiosResponse } from 'axios';
+
+const myContext = createContext({});
+
+export default function Context(props: any) {
+  const [userObj, setUserObj] = useState<any>();
+
+  useEffect(() => {
+    axios.get('http://localhost:4000/getuser').then((res: AxiosResponse) => {
+      if (res.data) {
+        setUserObj(res.data);
+      }
+    });
+  }, []);
+
+  return (
+    <myContext.Provider value={userObj}>{props.children}</myContext.Provider>
+  );
+}
