6ec7d6377ca063c96e12f1ea152c707596438d03
Opdata <pb03641@gmail.com>
2021-05-25 16:15:56 +0900

    LoginButton onClick Event(Google Login Open) Add

--- client/src/Components/LoginPage/Loginpage.tsx
+++ client/src/Components/LoginPage/Loginpage.tsx
@@ -1,11 +1,16 @@
 import googleIcon from '../../Assets/googleIcon.png';
 import styles from './Loginpage.module.css';
+
 export default function Loginpage() {
+  const googleLogin = () => {
+    window.open('http://localhost:4000/auth/google', '_self');
+  };
+
   return (
     <div className={styles.loginPage}>
       <h1>Login Page</h1>
       <div className={styles.loginForm}>
-        <div className={styles.googleContainer}>
+        <div className={styles.googleContainer} onClick={googleLogin}>
           <img src={googleIcon} alt="google Icon" />
           <p>Login With Google</p>
         </div>
